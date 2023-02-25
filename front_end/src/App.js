50ace232ec523d3931b0e334474258b02b461837
mohammed al maslamani <mw.iiiijob@gmail.com>
2023-02-25 17:57:46 +0300

    mohammad

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,12 +1,28 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
+import EditProfile from "./pages/profile/EditProfile";
 import Register from "./pages/register/Register";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
-  return <Register/>;
-}
+  return(
+  <div>
+    <BrowserRouter>
+  <Routes>
+    <Route path='/profile' element={< Profile/>}></Route>
+    <Route path='/editprofile' element={< EditProfile/>}></Route>
+    
+      
+   
+  </Routes>
+  </BrowserRouter>
 
+  </div>
+  );
+
+  }
 export default App;
