3986c86c9b0a28faf923ad301f705686938f6337
Mahmoud-harasis <mahmoudharasis8@gmail.com>
2023-02-25 14:47:22 +0300

    m

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-  return <Register/>;
+  return <Home/>;
 }
 
 export default App;
--- front_end/src/components/feed/Feed.jsx
+++ front_end/src/components/feed/Feed.jsx
@@ -6,12 +6,13 @@ import { Posts } from "../../dummyData/dummyData";
 export default function Feed() {
   return (
     <div className="feed">
-      <div className="feedWrapper">
+    
+      
         <Share />
         {Posts.map((p) => (
           <Post key={p.id} post={p} />
         ))}
       </div>
-    </div>
+      
   );
 }
