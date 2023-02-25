876dd5c02e0d2e054849ef93abfb4e1ca1658c31
Mahmoud-harasis <mahmoudharasis8@gmail.com>
2023-02-25 11:33:45 +0300

    messages

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -26,72 +26,129 @@ export default function Rightbar({ profile }) {
   const ProfileRightbar = () => {
     return (
       <>
-        <h4 className="rightbarTitle">User information</h4>
-        <div className="rightbarInfo">
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">City:</span>
-            <span className="rightbarInfoValue">New York</span>
-          </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">From:</span>
-            <span className="rightbarInfoValue">Madrid</span>
-          </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">Relationship:</span>
-            <span className="rightbarInfoValue">Single</span>
-          </div>
-        </div>
-        <h4 className="rightbarTitle">User friends</h4>
-        <div className="rightbarFollowings">
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/1.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/2.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/3.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/4.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/5.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/6.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
-        </div>
+        <div className="right">
+  <div className="messages">
+    <div className="heading">
+      <h4>Messages</h4>
+      <span><i className="uil uil-edit"></i></span>
+    </div>
+    <div className="search-bar">
+  <span><i className="uil uil-search"></i></span>
+  <input type="search" placeholder="Search Messages" id="message-search" />
+</div>
+
+<div className="category">
+  <h6 className="active">Primary</h6>
+  <h6>General</h6>
+  <h6 className="message-requests">Requests(7)</h6>
+</div>
+<div className="message">
+  <div className="profile-pic">
+    <img src="images/profile-17.jpg" />
+    <div className="active"></div>
+  </div>
+  <div className="message-body">
+    <h5>Kareena Joshua</h5>
+    <p className="text-muted">Just woke up bruh..</p>
+  </div>
+</div>
+<div className="message">
+  <div className="profile-pic">
+    <img src="images/profile-18.jpg" />
+    <div className="active"></div>
+  </div>
+  <div className="message-body">
+    <h5>Dan Smith</h5>
+    <p className="text-bold">2 New Messages</p>
+  </div>
+</div>
+<div className="message">
+  <div className="profile-pic">
+    <img src="images/profile-15.jpg" />
+  </div>
+  <div className="message-body">
+    <h5>Chris Brown</h5>
+    <p className="text-muted">Lol u right</p>
+  </div>
+</div>
+<div className="message">
+  <div className="profile-pic">
+    <img src="images/profile-14.jpg" />
+  </div>
+  <div className="message-body">
+    <h5>Lana Rose</h5>
+    <p className="text-bold">Birthday tomorrow!!</p>
+  </div>
+</div>
+<div className="message">
+  <div className="profile-pic">
+    <img src="images/profile-11.jpg" />
+  </div>
+  <div className="message-body">
+    <h5>Varun Nair</h5>
+    <p className="text-muted">Ssup?</p>
+  </div>
+</div>
+<div className="message">
+  <div className="profile-pic">
+    <img src="images/profile-1.jpg" />
+    <div className="active"></div>
+  </div>
+  <div className="message-body">
+    <h5>Jahnvi Doifode</h5>
+    <p className="text-bold">3 New Messages</p>
+  </div>
+</div>
+</div>
+</div><div className="friend-requests">
+  <h4>Requests</h4>
+  <div className="request">
+    <div className="info">
+      <div className="profile-pic">
+        <img src="images/profile-13.jpg" alt="Profile" />
+      </div>
+      <div>
+        <h5>Wilson Fisk</h5>
+        <p className="text-muted">8 mutual friends</p>
+      </div>
+    </div>
+    <div className="action">
+      <button className="btnm btnm-primary">Accept</button>
+      <button className="btnm">Decline</button>
+    </div>
+  </div>
+  <div className="request">
+    <div className="info">
+      <div className="profile-pic">
+        <img src="images/profile-20.jpg" alt="Profile" />
+      </div>
+      <div>
+        <h5>Srishti Tirkey</h5>
+        <p className="text-muted">2 mutual friends</p>
+      </div>
+    </div>
+    <div className="action">
+      <button className="btnm btnm-primary">Accept</button>
+      <button className="btnm">Decline</button>
+    </div>
+  </div>
+  <div className="request">
+    <div className="info">
+      <div className="profile-pic">
+        <img src="images/profile-5.jpg" alt="Profile" />
+      </div>
+      <div>
+        <h5>Christ Kahea</h5>
+        <p className="text-muted">1 mutual friend</p>
+      </div>
+    </div>
+    <div className="action">
+      <button className="btnm btnm-primary">Accept</button>
+      <button className="btnm">Decline</button>
+    </div>
+  </div>
+</div>
+
       </>
     );
   };
