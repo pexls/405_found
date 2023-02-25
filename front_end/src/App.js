6d8cf994281c0fd5b8014c5ed9b953ed9eff14b3
Ahmadzytoon <ahmadzytoon@outlook.com>
2023-02-25 22:21:37 +0300

    ahmad

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,24 +1,29 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SingleGroup from "./pages/Groups/SingleGroup"
 import { Routes, Route } from "react-router-dom";
+import Groups from "./pages/Groups/Groups";
 
 
 
 function App() {
   return(
-    <div>
+    <>
   <Routes>
      <Route path="/home" element={ <Home/> } />
      <Route path="/" element={ <Login/> } />
      <Route path="/register" element={ <Register/> } />
+     <Route path="/SingleGroup" element={ <SingleGroup/> } />
+     <Route path="/Profile" element={ <Profile/> } />
+     <Route path="/group" element={ <Groups/> } />
 
    </Routes>
-</div>
+</>
 )
 
 }
-
 
 export default App;
--- front_end/src/components/topbar/Topbar.jsx
+++ front_end/src/components/topbar/Topbar.jsx
@@ -6,7 +6,7 @@ export default function Topbar() {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <img className="logo" src="images/Untitled-1.png" alt="" />
+        <span className="logo">Lamasocial</span>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
@@ -18,11 +18,14 @@ export default function Topbar() {
         </div>
       </div>
       <div className="topbarRight">
-        
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
         <div className="topbarIcons">
           <div className="topbarIconItem">
             <BsFillPersonFill />
-            <span className="topbarIconBadge">2</span>
+            <span className="topbarIconBadge">1</span>
           </div>
           <div className="topbarIconItem">
             <BsFillChatFill />
